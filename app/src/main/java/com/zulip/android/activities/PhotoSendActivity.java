fa6a4de7d3d9d69f2cba12b410b2c14f559085f2
Sam <saumya.bhatnagar.sb@gmail.com>
2016-12-24 15:56:34 +0530

    Set captured image as background in PhotoSendActivity.

--- app/src/main/java/com/zulip/android/activities/PhotoSendActivity.java
+++ app/src/main/java/com/zulip/android/activities/PhotoSendActivity.java
@@ -1,12 +1,18 @@
 package com.zulip.android.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.WindowManager;
+import android.widget.ImageView;
 
 import com.zulip.android.R;
+import com.zulip.android.util.PhotoHelper;
 
 public class PhotoSendActivity extends AppCompatActivity {
+
+    private String mPhotoPath;
+    private ImageView mImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +22,16 @@ public class PhotoSendActivity extends AppCompatActivity {
         // run activity in full screen mode
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        // get the file path sent from ZulipActivity
+        final Intent intent = getIntent();
+        mPhotoPath = intent.getStringExtra(Intent.EXTRA_TEXT);
+
+        mImageView = (ImageView) findViewById(R.id.photoImageView);
+    }
+
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        PhotoHelper.setPic(mImageView, mPhotoPath, true);
     }
 }
