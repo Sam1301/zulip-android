baa4352cd6770efb432450bbd793673313690690
Sam <saumya.bhatnagar.sb@gmail.com>
2016-12-27 18:24:55 +0530

    Add different colors to marker tool.

--- app/src/main/java/com/zulip/android/activities/PhotoEditActivity.java
+++ app/src/main/java/com/zulip/android/activities/PhotoEditActivity.java
@@ -2,10 +2,15 @@ package com.zulip.android.activities;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.graphics.Point;
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Display;
+import android.view.View;
 import android.view.WindowManager;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
@@ -28,6 +33,18 @@ public class PhotoEditActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photo_edit);
+
+        // set a border and color for black marker color
+        ImageView black_marker = (ImageView) findViewById(R.id.black_marker);
+        GradientDrawable blackCircle = (GradientDrawable) black_marker.getDrawable();
+        blackCircle.setColor(ContextCompat.getColor(this, R.color.black_marker_tool));
+        blackCircle.setStroke(3, Color.GRAY);
+
+        // change background of marker tool to default color red on activity start up
+        int colorId = R.color.red_marker_tool;
+        ImageView markerIcon = (ImageView) findViewById(R.id.marker_btn);
+        GradientDrawable markerBackground = (GradientDrawable) markerIcon.getBackground();
+        markerBackground.setColor(ContextCompat.getColor(this, colorId));
 
         // run activity in full screen mode
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
@@ -69,5 +86,52 @@ public class PhotoEditActivity extends AppCompatActivity {
                 .load(mPhotoPath)
                 .asBitmap()
                 .into(mGlideTarget);
+    }
+
+    /**
+     * This function is called when any of the marker colors are chosen.
+     * Its sets the color for marker tool and changes its the background.
+     *
+     * @param view color ImageView
+     */
+    public void handleMarkerColorChange(View view) {
+        int colorId = R.color.red_marker_tool;
+        switch (view.getId()) {
+            case R.id.red_marker:
+                colorId = R.color.red_marker_tool;
+                break;
+            case R.id.yellow_marker:
+                colorId = R.color.yellow_marker_tool;
+                break;
+            case R.id.green_marker:
+                colorId = R.color.green_marker_tool;
+                break;
+            case R.id.white_marker:
+                colorId = R.color.white_marker_tool;
+                break;
+            case R.id.blue_marker:
+                colorId = R.color.blue_marker_tool;
+                break;
+            case R.id.black_marker:
+                colorId = R.color.black_marker_tool;
+                break;
+            default:
+                Log.e("Marker Tool", "Invalid color");
+                break;
+        }
+
+        // change marker tool color
+        mDrawCustomView.setBrushColor(ContextCompat.getColor(this, colorId));
+
+        // change background of marker tool
+        ImageView markerIcon = (ImageView) findViewById(R.id.marker_btn);
+        GradientDrawable markerBackground = (GradientDrawable) markerIcon.getBackground();
+        markerBackground.setColor(ContextCompat.getColor(this, colorId));
+        // if black color is selected, add a border to the background
+        if (colorId == R.color.black_marker_tool) {
+            markerBackground.setStroke(3, Color.GRAY);
+        } else {
+            markerBackground.setStroke(0, Color.GRAY);
+        }
     }
 }
